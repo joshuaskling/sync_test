29483e74d44883dc11a5330822e69759c6da0c14
Josh <joshuaskling@users.noreply.github.com>
2023-11-03 14:29:48 +1100

    Create basic2.groovy

---
+++ procedures/basic2.groovy
@@ -0,0 +1,9 @@
+procedure 'basic2_procedure', {
+  description = ''
+  jobNameTemplate = ''
+  projectName = 'Default'
+  resourceName = ''
+  timeLimit = '0'
+  timeLimitUnits = 'minutes'
+  workspaceName = ''
+}
