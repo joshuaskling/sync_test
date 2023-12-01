f3aa78d50b685cd9f68a2315ced22c7ffcc26708
Josh <joshuaskling@users.noreply.github.com>
2023-12-01 15:51:52 -0800

    Create new_procedure_1.groovy

---
+++ projects/test_project/procedures/new_procedure_1.groovy
@@ -0,0 +1,9 @@
+procedure 'new_procedure_1', {
+  description = ''
+  jobNameTemplate = ''
+  projectName = 'test_project'
+  resourceName = ''
+  timeLimit = '0'
+  timeLimitUnits = 'minutes'
+  workspaceName = ''
+}
